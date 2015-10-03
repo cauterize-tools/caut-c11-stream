29e68d91d7b569e77b3279a6cdbf03d0ad6daabe
John Van Enk <vanenkj@gmail.com>
2015-10-03 11:37:22 -0700

    Start generating the bare pieces of HFile

--- bin/Main.hs
+++ bin/Main.hs
@@ -1,4 +1,45 @@
 module Main where
 
+import Cauterize.C11Stream.Options
+import Cauterize.C11Stream.HFile
+import Data.Text (unpack)
+import System.Directory
+import System.FilePath.Posix
+import qualified Cauterize.Specification as S
+
 main :: IO ()
-main = putStrLn "caut-c11-stream"
+main = runWithOptions caut2c11
+
+caut2c11 :: Caut2C11Opts -> IO ()
+caut2c11 (Caut2C11Opts { specFile = sf, outputDirectory = od }) = createGuard od $ do
+  spec <- loadSpec
+  let baseName = unpack $ S.specName spec
+
+  generateDynamicFiles od baseName spec
+  where
+    loadSpec :: IO S.Specification
+    loadSpec = do
+      s <- S.parseSpecificationFromFile sf
+      case s of
+        Left e -> error $ show e
+        Right s' -> return s'
+
+createGuard :: FilePath -> IO a -> IO a
+createGuard out go = do
+  fe <- doesFileExist out
+  de <- doesDirectoryExist out
+
+  if fe
+    then error $ "Error: " ++ out ++ " is a file."
+    else if de
+          then go
+          else createDirectory out >> go
+
+generateDynamicFiles :: FilePath -> String -> S.Specification -> IO ()
+generateDynamicFiles path baseName spec = do
+  writeFile (path `combine` (baseName ++ ".h")) (hFileFromSpec spec)
+--  writeFile (path `combine` (baseName ++ ".c")) (cFileFromSpec spec)
+--  writeFile (path `combine` (baseName ++ "_message.h")) (hMessageFileFromSpec spec)
+--  writeFile (path `combine` (baseName ++ "_message.c")) (cMessageFileFromSpec spec)
+--  writeFile (path `combine` "test_client.h")  (testClientFromSpec spec)
+--  writeFile (path `combine` "Makefile")  (makefileFromSpec spec)
--- lib/Cauterize/C11Stream/HFile.hs
+++ lib/Cauterize/C11Stream/HFile.hs
@@ -1,1 +1,20 @@
-module Cauterize.C11Stream.HFile where
+{-# LANGUAGE QuasiQuotes #-}
+module Cauterize.C11Stream.HFile
+       ( hFileFromSpec
+       ) where
+
+import qualified Cauterize.Specification as S
+
+import Data.Text (unpack)
+import Data.String.Interpolate
+import Data.String.Interpolate.Util
+
+hFileFromSpec :: S.Specification -> String
+hFileFromSpec s = unindent [i|
+  #ifndef #{guardSym}
+  #define #{guardSym}
+  #endif /* #{guardSym} */
+|]
+  where
+    guardSym = [i|_CAUTERIZE_C11STREAM_#{ln}_|]
+    ln = unpack (S.specName s)
