250a85fb2c948efdafa34a697685c7469df35d58
John Van Enk <vanenkj@gmail.com>
2015-11-03 13:57:59 -0700

    Fill in descriptors. Need field tables.

--- lib/Cauterize/C11Stream/CDescriptors.hs
+++ lib/Cauterize/C11Stream/CDescriptors.hs
@@ -11,12 +11,13 @@ import Data.Text (unpack)
 import Data.List (intercalate)
 
 import qualified Cauterize.Specification as S
+import qualified Cauterize.CommonTypes as C
 
 cDescriptorsFromSpec :: S.Specification -> String
 cDescriptorsFromSpec s = unindent [i|
   #include "#{ln}_descriptors.h"
 
-  struct type_descriptor type_descriptors[TYPE_COUNT_#{ln}] = {
+  struct type_descriptor const type_descriptors[TYPE_COUNT_#{ln}] = {
 #{descriptorList}
   };
 
@@ -33,7 +34,7 @@ cDescriptorsFromSpec s = unindent [i|
     {
       .name = "#{n}",
       .index = type_index_#{ln}_#{n},
-      .prototype_tag = prototype_#{tps},
+      .prototype_tag = caut_proto_#{tps},
       .prototype.#{tps} = {
 #{prototypeBody s t}
       },
@@ -53,42 +54,50 @@ typeToPrimString S.Type { S.typeDesc = d } = n
       S.Combination {} -> "combination"
       S.Union {}       -> "union"
 
+--prototypeFields :: S.Specification
+
 prototypeBody :: S.Specification -> S.Type -> String
-prototypeBody s (S.Type { S.typeDesc = d }) =
+prototypeBody s (S.Type { S.typeName = n, S.typeDesc = d }) =
   case d of
     S.Synonym { S.synonymRef = r }
       -> chompNewline [i|
         .ref_index = type_index_#{ln}_#{ident2str r},|]
-    S.Range {}
+    S.Range { S.rangeOffset = ro, S.rangeLength = rl, S.rangeTag = rt }
       -> chompNewline [i|
-        .offset = ???,
-        .length = ???,
-        .tag = ???,|]
-    S.Array { S.arrayRef = r }
-      -> chompNewline [i|
-        .ref_index = type_index_#{ln}_#{ident2str r},
-        .length = ???,|]
-    S.Vector { S.vectorRef = r}
+        .offset = #{ro},
+        .length = #{rl},
+        .tag = #{tagToTagEnumStr rt},|]
+    S.Array { S.arrayRef = r, S.arrayLength = al }
       -> chompNewline [i|
         .ref_index = type_index_#{ln}_#{ident2str r},
-        .max_length = ???,|]
-    S.Enumeration {}
+        .length = #{al},|]
+    S.Vector { S.vectorRef = r, S.vectorLength = vl }
       -> chompNewline [i|
-        .tag = ???,
-        .length = ???,|]
-    S.Record {}
+        .ref_index = type_index_#{ln}_#{ident2str r},
+        .max_length = #{vl},|]
+    S.Enumeration { S.enumerationTag = et, S.enumerationValues = evs }
       -> chompNewline [i|
-        .field_count = ???,
-        .fields = ???,|]
-    S.Combination {}
+        .tag = #{tagToTagEnumStr et},
+        .length = #{length evs},|]
+    S.Record { S.recordFields = rs }
       -> chompNewline [i|
-        .tag = ???,
-        .field_count = ???,
-        .fields = ???,|]
-    S.Union {}
+        .field_count = #{length rs},
+        .fields = record_fields_#{ln}_#{ident2str n},|]
+    S.Combination { S.combinationFields = cf, S.combinationTag = ct }
       -> chompNewline [i|
-        .tag = ???,
-        .field_count = ???,
-        .fields = ???,|]
+        .tag = #{tagToTagEnumStr ct},
+        .field_count = #{length cf},
+        .fields = combination_fields_#{ln}_#{ident2str n},|]
+    S.Union { S.unionFields = uf, S.unionTag = ut }
+      -> chompNewline [i|
+        .tag = #{tagToTagEnumStr ut},
+        .field_count = #{length uf},
+        .fields = union_fields_#{ln}_#{ident2str n},|]
   where
     ln = unpack (S.specName s)
+
+tagToTagEnumStr :: C.Tag -> String
+tagToTagEnumStr C.T1 = "caut_tag_1"
+tagToTagEnumStr C.T2 = "caut_tag_2"
+tagToTagEnumStr C.T4 = "caut_tag_4"
+tagToTagEnumStr C.T8 = "caut_tag_8"
