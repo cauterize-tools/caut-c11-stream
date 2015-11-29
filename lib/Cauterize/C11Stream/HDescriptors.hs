8c99f2631a87d537fe77d620bc08312297329020
John Van Enk <vanenkj@gmail.com>
2015-11-29 11:51:25 -0500

    extern the schema_descriptor.

--- lib/Cauterize/C11Stream/HDescriptors.hs
+++ lib/Cauterize/C11Stream/HDescriptors.hs
@@ -31,6 +31,9 @@ hDescriptorsFromSpec s = unindent [i|
   /* an index for each type */
 #{typeIndiciesEnum}
 
+  /* a descriptor for the entire schema */
+  extern struct schema_descriptor const schema_descriptor_#{ln};
+
   /* an array of type descriptors */
   extern struct type_descriptor const type_descriptors_#{ln}[TYPE_COUNT_#{ln}];
 
