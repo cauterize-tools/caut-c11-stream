932e1746becc495b19f5d7aa058bc16c0956297b
John Van Enk <vanenkj@gmail.com>
2015-12-04 16:19:21 -0800

    Different crucible options.

--- run_crucible.sh
+++ run_crucible.sh
@@ -2,10 +2,9 @@
 
 stack exec crucible -- tester \
   --build-cmd="stack exec caut-c11-stream -- --spec=%s --output=c11" \
-  --build-cmd="cd c11 && ls *.c && clang -Wall -Wextra -std=c11 -pedantic *.c -o test_client" \
+  --build-cmd="cd c11 && ls *.c && clang -Os -Wall -Wextra -std=c11 -pedantic *.c -o test_client" \
   --run-cmd="./c11/test_client" \
-  --schema-count=10 \
-  --instance-count=1000 \
+  --schema-count=1 \
+  --instance-count=100 \
   --type-count=100 \
-  --enc-size=4096 \
-  --prototypes=synonym,array,vector,record,combination,union,enumeration,range
+  --enc-size=1048576
