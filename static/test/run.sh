19d450da9b4729e82571159e95920ac6136685b4
John Van Enk <vanenkj@gmail.com>
2016-02-05 12:27:10 -0800

    Update the test runner to use the new cauterize command line args.

--- static/test/run.sh
+++ static/test/run.sh
@@ -6,7 +6,7 @@ CFLAGS="-O3 -g3 -Wall -Wextra --std=c11 -pedantic ${SANITIZE}"
 INCLUDES="-Igenerated/"
 
 stack build
-stack exec cauterize -- --schema caut_test.schema --output caut_test.spec
+stack exec cauterize -- --schema caut_test.schema --specification caut_test.spec
 stack exec caut-c11-stream -- --spec caut_test.spec --output generated
 
 clang ${CFLAGS} ${INCLUDES} -Os -o ${OUT_BIN} \
