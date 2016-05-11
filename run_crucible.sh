2adaf36bad0fa53009841c35155d2dadcd4a8398
John Van Enk <vanenkj@gmail.com>
2016-05-11 14:09:38 -0700

    Update the crucible runner to use the new argument setup.

--- run_crucible.sh
+++ run_crucible.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 stack exec crucible -- tester \
-  --build-cmd="stack exec caut-c11-stream -- --spec=%s --output=c11" \
+  --build-cmd="stack exec caut-c11-stream -- genall %s c11" \
   --build-cmd="cd c11 && ls *.c && clang -Os -Wall -Wextra -std=c11 -pedantic *.c -o test_client" \
   --run-cmd="./c11/test_client" \
   --schema-count=1 \
