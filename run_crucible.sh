3c6397a5fd847cf6d306be9b9ff9ae25defaea4b
Jay Kickliter <jay@kickliter.com>
2017-03-29 16:27:45 -0700

    Update crucible runner to use the COMPILER environment variable on travis

--- run_crucible.sh
+++ run_crucible.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+COMPILER="${COMPILER:-cc}"
+
 stack exec crucible -- tester \
   --build-cmd="stack exec caut-c11-stream -- genall %s c11" \
-  --build-cmd="cd c11 && ls *.c && clang -Os -Wall -Wextra -std=c11 -pedantic *.c -o test_client" \
+  --build-cmd="cd c11 && ls *.c && $COMPILER -Os -Wall -Wextra -std=c11 -pedantic *.c -o test_client" \
   --run-cmd="./c11/test_client" \
   --schema-count=1 \
   --instance-count=1000 \
